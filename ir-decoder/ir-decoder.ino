0d9d95433f4949779f78a0541c86dc37b79dd7c7
Deluca-syntax-error <rojito.jigsaw@gmail.com>
2025-10-26 15:57:58 -0300

    Updated ir-decoder.ino, now can hold buttons

--- ir-decoder/ir-decoder.ino
+++ ir-decoder/ir-decoder.ino
@@ -1,18 +1,12 @@
-#include <string.h>
-
-// 0x00 Super key
-// 0x01 escape
-// 0x02 enter
-// 0x03 fullscreen
-// 0x04 page up
-// 0x05 page down
-// 0x06 volume+
-// 0x07 volume-
-
 const int irPin = 9;
+int lastKey = 0;
+unsigned long lastKeyTime = 0;
 
-//                Power-  up - left  -right -down- OK - mute   - 1 -    2 -    3 -    4 -     5 -    6 -    7 -    8 -    9 -     0 -  ch+-   ch- - vol+ - vol-
-int keymap[21] = {11789, 2034, 18930, 19443, 2545, 3058, 14348, 16907, 17419, 17932, 18443, 18956, 19468, 19981, 20491, 21004, 16394, 12812, 1229, 15373, 15886};
+const unsigned long holdDelay = 300;   
+const unsigned long repeatInterval = 100;
+const unsigned long minInterval = 300; // debounce para no enviar dos veces
+
+bool isHolding = false;
 
 void setup() {
   Serial.begin(115200);
@@ -20,140 +14,50 @@ void setup() {
 }
 
 void loop() {
-	int key = getIrKey();
-  
+  unsigned long now = millis();
+  int key = getIrKey();
 
-  
-	if (key != 0) 
-	{
-		Serial.println(key);
-	}
-    /*for (int i = 0; i < 21; i++) {
-      if (key == keymap[i]) {
-        switch (keymap[i]) {
-          case 11789:
-            exe("shutdown /s /t 0");
-            break;
-          case 16907:
-            exe("vk");
-            break;
-          case 17419:
-            exe("chrome");
-            break;
-          case 17932: 
-	    
-	  case 18443: 
-
-	  case 18596: 
-
-	  case 19468: 
-	  
-	  case 19981: 
-
-	  case 20491: 
-	
-	  case 21004: 
-	  
-	  case 16394:
-          case 3058:
-            move(0x12);
-            break;
-          case 15373:
-            vol('+', 5);
-            break;
-          case 15886:
-            vol('-', 5);
-            break;
-          case 18930:
-            move(0x10);
-            break;
-          case 19443:
-            move(0x11);
-            break;
-          case 2034:
-            move(0x08);
-            break;
-          case 2545:
-            move(0x09);
-            break;
-	  case 14348:
-	    move(0x13);
-	    break;
-        }
-        break; // Salir del for cuando encuentre la tecla
+  if (key != 0) {
+    if (key != lastKey) {
+      Serial.println(key);
+      lastKey = key;
+      lastKeyTime = now;
+      isHolding = false;
+    } else {
+      // mismo botón
+      if (!isHolding && now - lastKeyTime > holdDelay) {
+        isHolding = true;
+        lastKeyTime = now;
+      }
+      if (isHolding && now - lastKeyTime >= repeatInterval) {
+        Serial.println(key);
+        lastKeyTime = now;
       }
     }
-  }*/
-  
-  delay(150); // CAMBIO: era 1000, ahora 150
+  } else {
+    // botón liberado
+    lastKey = 0;
+    isHolding = false;
+  }
 }
 
 int getIrKey() {
-  int len = pulseIn(irPin, LOW);
-  int key, temp;
-  key = 0;
-  
-  if (len > 5000) {
+  long len = pulseIn(irPin, LOW);
+  int key = 0;
+  long temp;
+
+  if (len > 5000) { // código completo
     for (int i = 1; i <= 32; i++) {
       temp = pulseIn(irPin, HIGH);
-      if (temp > 1000)
-        key = key + (1 << (i - 17));
+      if (temp > 1000) key = key + (1 << (i - 17));;
     }
+    if(key<0) key = -key;
+    
+    return key;
+  } else if (len > 500) { // código corto (hold)
+    return lastKey;
   }
-  
-  if (key < 0)
-    key = -key;
-  
-  delay(100); // CAMBIO: era 250, ahora 100
-  return key;
-}
 
-int move(byte i) {
-  Serial.write(i);
-  delay(200); // CAMBIO: era 500, ahora 200
   return 0;
 }
 
-int sendText(char* tx) {
-  Serial.write(tx);
-  Serial.write(0x02);
-  delay(200); // CAMBIO: era 500, ahora 200
-  return 0;
-}
-
-int exe(char* tx) {
-  Serial.write(0x00);
-  delay(200); // CAMBIO: era 500, ahora 200
-  Serial.write("r");
-  delay(200); // CAMBIO: era 500, ahora 200
-  Serial.write(tx);
-  delay(200); // CAMBIO: era 500, ahora 200
-  Serial.write(0x02);
-  delay(200); // CAMBIO: era 500, ahora 200
-  return 0;
-}
-
-int ful() {
-  Serial.write(0x03);
-  delay(200); // CAMBIO: era 500, ahora 200
-  return 0;
-}
-
-int vol(char v, int cant) {
-  byte vol;
-  if (v == '+') {
-    vol = 0x06;
-  } else if (v == '-') {
-    vol = 0x07;
-  }
-  
-<<<<<<< HEAD
-  for (int i = 0; i < cant; i++) { // ARREGLADO: faltaba i=0
-    Serial.write(vol);
-    delay(150); // CAMBIO: era 300, ahora 150
-  }
-  return 0;
-}
-=======
-}
->>>>>>> master
